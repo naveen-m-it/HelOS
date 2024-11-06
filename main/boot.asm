d03fc227dad51b9954b592d2a190da258cfbbeb8
Naveen M <47354553+naveen-m-it@users.noreply.github.com>
2024-11-06 21:51:14 +0530

    master

--- main/boot.asm
+++ main/boot.asm
@@ -1,186 +1,177 @@
-;------------------------------------------------------------------------------------------------------------
-; helOS! 5-11-2024
-;------------------------------------------------------------------------------------------------------------
-[org 0x7c00]
-_start:
-    call _clear_screen
-    call print_title
-    call get_command
+;===============================================
+; helOS! 6-11-24
+;===============================================
+[org 0x7c00]                        ; set origin to position 0x7c00
+start:
+    ;call _clear                     ; Clear screen
+    call print_title                ; print title text
+    call get_command                ; call get_command function
+    ;call _print_debug_msg
+    jmp end                         ; jump to end
 
-;------------------------------------------------------------------------------------------------------------
-; title
-;------------------------------------------------------------------------------------------------------------
+;===============================================
+; Print functions
+;===============================================
 
-;------------------------------------------------------------------------------------------------------------
-; get command from user
-;------------------------------------------------------------------------------------------------------------
-get_command:
-    call _print_command_tag
-    .loop:
-        call _read_char
-        cmp al, 0x0d
-        je _process_command
-        mov si, command_buffer_len
-        cmp si, 63
-        je get_command.end
-        mov [command_buffer + si], al
-        inc byte [command_buffer_len]
-        call _print_char
-        jmp get_command.loop
-    .end:
-        ret
-
-;------------------------------------------------------------------------------------------------------------
-; Command process
-;------------------------------------------------------------------------------------------------------------
-_process_command:
-    mov si, command_buffer
-    
-    mov di, clear_cmd
-    call _compare_command
-    je _clear_screen
-
-    mov di, help_cmd
-    call _compare_command
-    je _help_screen
-
-    mov di, version_cmd
-    call _compare_command
-    je _version_screen
-
-    mov di, exit_cmd
-    call _compare_command
-    je _end
-
-    call _clear_buffer
-    call _clear_screen
-    jmp get_command
-
-_compare_command:
-    pusha
-    mov cx, 64
-    .loop:
-        mov al, [si]
-        cmp al, [di]
-        jne _compare_command.not_equal
-        cmp al, 0
-        je _compare_command.equal
-        inc si
-        inc di
-        loop _compare_command.loop
-
-    .not_equal:
-        xor ax, ax
-        popa
-        ret
-
-    .equal:
-        mov ax, 1
-        popa
-        ret
-
-;------------------------------------------------------------------------------------------------------------
-; printing functions
-;------------------------------------------------------------------------------------------------------------
-_print_command_tag:
-    mov si, command_tag
-    .loop:
-        mov al, [si]
-        cmp al, 0
-        je _print_command_tag.end
-        call _print_char
-        inc si
-        jmp _print_command_tag.loop
-    .end:
-        ret
-
+; Title text
 print_title:
-    mov si, title
+    mov si, msg                     ; load msg memory address to si register
+    .loop:
+        mov al, [si]                ; load si memory content to al register
+        cmp al, 0                   ; compare al to 0
+        je .end                     ; if equal jump to local end label
+        call _print_char            
+        inc si                      ; increase si by 1
+        jmp .loop                   ; jump to local loop label
+    .end:
+        ret                         ; return control to caller and executes next instruction
+
+print_command_tag:
+    mov si, command_tag
     .loop:
         mov al, [si]
         cmp al, 0
         je .end
         call _print_char
         inc si
-        jmp print_title.loop
+        jmp .loop
     .end:
         ret
 
-_print_buffer:
+;===============================================
+; Command process
+;===============================================
+
+; Getting command
+get_command:
+    call print_command_tag
+    .loop:
+        call _read_char
+        cmp al, 0xd
+        je _process_command
+        mov si, command_buffer_len
+        cmp si, 63
+        je .end
+        mov byte [command_buffer + si], al
+        inc byte [command_buffer_len]
+        call _print_char
+        jmp .loop
+    .end:
+        jmp .loop
+
+; Processing command
+_process_command:
+
     mov si, command_buffer
+    mov di, exit_cmd
+    call _compare_command
+    cmp ax, 1
+    je _exit
+        
+    mov si, command_buffer
+    mov di, clear_cmd
+    call _compare_command
+    cmp ax, 1
+    je _clear
+
+    mov si, command_buffer
+    mov di, help_cmd
+    call _compare_command
+    cmp ax, 1
+    je _help
+
+    jmp _invalid_command
+
+_end_process:
+    call _clear_command_buffer
+    jmp get_command
+
+; Compares commands 
+_compare_command:
+    pusha
+    mov cx, 64
     .loop:
         mov al, [si]
+        cmp al, [di]
+        jne .not_equal
         cmp al, 0
-        je _print_buffer.end
-        call _print_char
+        je .equal
         inc si
-        jmp _print_buffer.loop
-    .end:
+        inc di
+        loop .loop
+    .equal:
+        mov ax, 1
+        popa
+        ret
+    .not_equal:
+        xor ax, ax
+        popa
         ret
 
-;------------------------------------------------------------------------------------------------------------
-; Utility functions
-;------------------------------------------------------------------------------------------------------------
-_clear_screen:
+;===============================================
+; Util functions
+;===============================================
+
+; Clear function
+_clear:
     mov ax, 0x0600
     mov bh, 0x07
     mov cx, 0
-    mov dx, 0x184f
+    mov dx, 0x0184f
     int 0x10
+
     mov ah, 0x02
     mov bh, 0
     mov dh, 0
     mov dl, 0
     int 0x10
-    ret
+    
+    mov ah, 0x0003
+    int 0x10
+    
+    jmp _end_process
 
-_help_screen:
+; Help function 
+_help:
     mov si, help_msg
     .loop:
         mov al, [si]
         cmp al, 0
-        je _help_screen.end
+        je .end
         call _print_char
         inc si
-        jmp _help_screen.loop
+        jmp .loop
     .end:
-        ret
+        jmp _end_process
 
-_version_screen:
-    mov si, version_msg
+; Exit function
+_exit:
+    mov si, exit_msg
     .loop:
         mov al, [si]
         cmp al, 0
-        je _version_screen.end
+        je .end
         call _print_char
         inc si
-        jmp _version_screen.loop
+        jmp .loop
     .end:
-        ret
+        jmp end                     ; jump to end of the function
 
-_error_screen: 
+; invalid handle
+_invalid_command:
     mov si, error_msg
     .loop:
         mov al, [si]
         cmp al, 0
-        je _error_screen.end
+        je .end
         call _print_char
         inc si
-        jmp _error_screen.loop
+        jmp .loop
     .end:
-        ret
+        jmp _end_process
 
-_print_char:
-    mov ah, 0x0e
-    int 0x10
-    ret
-
-_read_char:
-    mov ah, 0x00
-    int 0x16
-    ret
-
-_clear_buffer:
+; Clears command buffer and reset length to 0
+_clear_command_buffer:
     mov cx, 64
     mov di, command_buffer
     mov al, 0
@@ -188,28 +179,72 @@ _clear_buffer:
     mov byte [command_buffer_len], 0
     ret
 
-_end:
+; Print character
+_print_char:
+    mov ah, 0x0e
+    int 0x10
+    ret
+
+; Read character from keyboard input
+_read_char:
+    mov ah, 0x00
+    int 0x16
+    ret
+
+;===============================================
+; Debugging
+;===============================================
+
+_print_debug_msg:
+    mov si, debug_msg
+    .loop:
+        mov al, [si]
+        cmp al, 0
+        je .end
+        call _print_char
+        inc si
+        jmp .loop
+    .end:
+        ret
+
+_print_equal_msg:
+    mov si, equal_msg
+    .loop:
+        mov al, [si]
+        cmp al, 0
+        je .end
+        call _print_char
+        inc si
+        jmp .loop
+    .end:
+        ret
+
+;++++++++++++++++++++++++++++++++++++++++++++++++
+; End of program
+;++++++++++++++++++++++++++++++++++++++++++++++++
+end:
     jmp $
-;------------------------------------------------------------------------------------------------------------
-; data storage for messages and commands
-;------------------------------------------------------------------------------------------------------------
 
-;Strings
-title db "HelOS!", 0
-command_tag db 0xd, 0xa, ">>> ", 0
-help_cmd db "help", 0
+;===============================================
+; Messages and buffers
+;===============================================
+
+msg db "helOS!", 0xd, 0xa, 0
+command_tag db ">>> ", 0
+
 clear_cmd db "clear", 0
+help_cmd db "help", 0
 exit_cmd db "exit", 0
-version_cmd db "version", 0
-error_msg db "unknown command!", 0xd, 0xa, 0
-version_msg db "helOS version - 0.01", 0xd, 0xa, 0
 
-help_msg db 0xd, 0xa, "help - help", 0xd, 0xa, "version - version",0xd, 0xa,"clear - clear screen",0xd, 0xa,"exit - exit from terminal",0xd, 0xa, 0
+help_msg db "Help: Type 'clear' to clear screen.", 0xd, 0xa, "Type 'help' for this message.", 0xd, 0xa, 0
+error_msg db 0xd, 0xa, "Invalid command.", 0xd, 0xa, 0
+exit_msg db "Exited!", 0xd, 0xa, 0
+debug_msg db "Reached!", 0xd, 0xa, 0
+equal_msg db "Equal reached!", 0xd, 0xa, 0
 
-;buffers
 command_buffer db 64
 command_buffer_len db 0
 
-; fill 512 bytes
+; fill remaining empty memory to 0
 times 510-($-$$) db 0
 dw 0xaa55
